bdf78c86d4357b2247c7a1680bcf51f95552abb4
valentintietz-bit <valentintietz@gmail.com>
2025-09-02 10:50:38 +0200

    Update Strobe_light_fountain.ino

--- Strobe_light_fountain/Strobe_light_fountain.ino
+++ Strobe_light_fountain/Strobe_light_fountain.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+const int pinLED = 9;
+const int pinPompe = 3;
+const int pinPot = A1;
 
+unsigned long previousMicros = 0;
+unsigned long period_us = 100000; // 10 Hz par défaut
+unsigned long ton_us = 5000;      // 5% dce 100000 µs
+
+bool ledState = false;
+
+void setup() {
+  pinMode(pinLED, OUTPUT);
+  pinMode(pinPompe, OUTPUT);
+  analogWrite(pinPompe, 240); // PWM 50% pour pompe
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int valPot = analogRead(pinPot);
+  int freq = map(valPot, 0, 1023, 25, 100);
+  period_us = 1000000UL / freq;
+  ton_us = period_us * 0.05;
 
+  unsigned long currentMicros = micros();
+
+  if (ledState && (currentMicros - previousMicros >= ton_us)) {
+    digitalWrite(pinLED, HIGH);
+    ledState = false;
+    previousMicros = currentMicros;
+  } else if (!ledState && (currentMicros - previousMicros >= (period_us - ton_us))) {
+    digitalWrite(pinLED, LOW);
+    ledState = true;
+    previousMicros = currentMicros;
+  }
 }
